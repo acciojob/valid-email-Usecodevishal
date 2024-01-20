a499bac4b8143d94e95d6bf1b774ee7da3e3e4c3
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2024-01-21 01:27:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function validEmail(str) {
   //your JS code here.
+	if(str === "") return false
+	let indOfRate = str.indexOf("@");
+	let indOfDot = str.indexOf(".");
+	let indOfCom = str.indexOf("com");
+	let indOfCo = str.indexOf(Co);
+	let indOfIn = str.indexOf("in");
+	if(indOfDot<indOfRate){
+		return false
+	}else{
+		return true
+	}
+	
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+//const str = prompt("Enter an email address.");
 alert(validEmail(str));
